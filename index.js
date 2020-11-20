53ec6d3c0e80482429a6a987fadb273fd053d8f8
Will Haslett <will.haslett@gmail.com>
2020-11-19 23:23:28 -0500

    JS registers switch clicks

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function goToTwitter() {
 function goToGitHub() {
   window.location.assign('https://github.com/willhaslett/covid-19-growth');
 }
+
+function toggleCountOf() {
+  console.log('toggling count-of');
+}
+function toggleMeasure() {
+  console.log('toggling measure');
+}
+
+(function () {
+  var switchCountOf = document.getElementById('switch-count-of');
+  var switchMeasure = document.getElementById('switch-measure');
+  switchCountOf.addEventListener('mousedown', toggleCountOf);
+  switchMeasure.addEventListener('mousedown', toggleMeasure);
+})();
